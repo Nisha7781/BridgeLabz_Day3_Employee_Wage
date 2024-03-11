7065352780f9fa1123270a98cd65732f4884a53f
Nisha7781 <nishamali0781@gmail.com>
2024-03-11 17:34:59 +0530

    Check employee attendance

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,5 +1,19 @@
 public class EmployeeWage {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Welcome to Employee Wage Computation Program....");
+        int is_present = 1;
+
+        double EmpPresent = Math.floor(Math.random() * 10) % 2;
+        System.out.println(EmpPresent);
+
+        if(EmpPresent == is_present)
+        {
+            System.out.println("Employee is present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
     }
 }
