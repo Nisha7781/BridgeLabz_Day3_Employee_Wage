977a452bdd0e9f1ecc442ff79a425047946b8d5c
Nisha7781 <nishamali0781@gmail.com>
2024-03-11 18:06:54 +0530

    Calculate employee wage till total working hrs or working days

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -2,15 +2,19 @@ public class EmployeeWage {
     public static int fullTime = 1;
     public static int partTime = 2;
     static int ratePerHr = 20;
-    static int workingDays = 20;
+    static int MaxWorkingDays = 20;
+    static int MaxWorkinghrs = 100;
 
     public static void main(String[] args)
     {
         System.out.println("Welcome to Employee Wage Computation Program....");
 
         int empHr = 0, empWage = 0, totalEmpWage =0;
-        for (int i=0; i < workingDays; i++)
+        int WorkHrs = 0, WorkDays = 0;
+
+        while(WorkDays < MaxWorkingDays && WorkHrs < MaxWorkinghrs )
         {
+            WorkDays++;
             int EmpPresent = (int) Math.floor(Math.random( )*10) % 3;
 
             if(EmpPresent == fullTime)
@@ -26,12 +30,16 @@ public class EmployeeWage {
                 empHr = 0;
             }
 
+            WorkHrs = WorkHrs + empHr;
+            System.out.println("Day :" + WorkDays + " Emp HR: "+empHr);
 
             empWage = empHr * ratePerHr;
-            totalEmpWage = totalEmpWage + empWage;
-            System.out.println("Emp Wage on day "+ i +" is " + empWage);
+            System.out.println("Employee Wage on day " + WorkDays +" is: " + empWage);
 
         }
-        System.out.println("Total Emp Wage:" + totalEmpWage);
+        System.out.println("Total working Days: "+WorkDays);
+        System.out.println("Total working hours: "+WorkHrs);
+        totalEmpWage = WorkHrs * ratePerHr;
+        System.out.println("Total Employee Wage:" + totalEmpWage);
     }
 }
