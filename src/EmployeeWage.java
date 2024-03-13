f6c4df9780a8e30a09479f6d2d55d968dc3ad7ac
Nisha7781 <nishamali0781@gmail.com>
2024-03-13 11:15:33 +0530

    Calculate employee wage for multiple companies.

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,16 +1,14 @@
 public class EmployeeWage {
     public static int fullTime = 1;
     public static int partTime = 2;
-    static int ratePerHr = 20;
-    static int MaxWorkingDays = 20;
-    static int MaxWorkinghrs = 100;
 
-    public void calculateWage()
+    public void calculateWage(String company, int ratePerHr,int maxWorkingDays, int maxWorkinghrs)
     {
-        int empHr = 0, empWage = 0, totalEmpWage =0;
-        int WorkHrs = 0, WorkDays = 0;
+        int empHr = 0, empWage = 0;
+        int totalEmpWage =0;
+        int WorkHrs = 0,  WorkDays = 0;
 
-        while(WorkDays < MaxWorkingDays && WorkHrs < MaxWorkinghrs )
+        while(WorkDays < maxWorkingDays && WorkHrs < maxWorkinghrs )
         {
             WorkDays++;
             int EmpPresent = (int) Math.floor(Math.random( )*10) % 3;
@@ -32,19 +30,25 @@ public class EmployeeWage {
             System.out.println("Day :" + WorkDays + " Emp HR: "+empHr);
 
             empWage = empHr * ratePerHr;
-            System.out.println("Employee Wage on day " + WorkDays +" is: " + empWage);
+
 
         }
+
+        System.out.println();
         System.out.println("Total working Days: "+WorkDays);
         System.out.println("Total working hours: "+WorkHrs);
         totalEmpWage = WorkHrs * ratePerHr;
-        System.out.println("Total Employee Wage:" + totalEmpWage);
+        System.out.println("Total Employee Wage of company " +company + " is "+ totalEmpWage);
+        System.out.println();
+
     }
     public static void main(String[] args)
     {
         System.out.println("Welcome to Employee Wage Computation Program....");
         EmployeeWage wage = new EmployeeWage();
-        wage.calculateWage();
+        wage.calculateWage("TATA", 20, 20, 100);
+        wage.calculateWage("QK", 18, 20, 120);
+        wage.calculateWage("Reliance", 24, 22, 150);
 
     }
 }
