4ddc8ab926189b9053e7e28bbdd2311ef95789a2
Nisha7781 <nishamali0781@gmail.com>
2024-03-13 12:48:29 +0530

    Calculate employee wage and save employee wage for each company

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -2,7 +2,22 @@ public class EmployeeWage {
     public static int fullTime = 1;
     public static int partTime = 2;
 
-    public void calculateWage(String company, int ratePerHr,int maxWorkingDays, int maxWorkinghrs)
+    private final String company;
+    private final int ratePerHr;
+    private final int maxWorkingDays;
+    private final int maxWorkinghrs;
+    private int totalEmpWage;
+
+    public EmployeeWage(String company, int ratePerHr,int maxWorkingDays, int maxWorkinghrs)
+    {
+        this.company = company;
+        this.ratePerHr = ratePerHr;
+        this.maxWorkingDays = maxWorkingDays;
+        this.maxWorkinghrs = maxWorkinghrs;
+    }
+
+
+    public void calculateWage()
     {
         int empHr = 0, empWage = 0;
         int totalEmpWage =0;
@@ -27,28 +42,35 @@ public class EmployeeWage {
             }
 
             WorkHrs = WorkHrs + empHr;
-            System.out.println("Day :" + WorkDays + " Emp HR: "+empHr);
-
             empWage = empHr * ratePerHr;
 
-
         }
-
-        System.out.println();
-        System.out.println("Total working Days: "+WorkDays);
-        System.out.println("Total working hours: "+WorkHrs);
         totalEmpWage = WorkHrs * ratePerHr;
-        System.out.println("Total Employee Wage of company " +company + " is "+ totalEmpWage);
-        System.out.println();
+    }
+
+    public String toString()
+    {
+        System.out.println("Details of " + company + " employee");
+        System.out.println("-----------------------------------------------------");
+        System.out.println("Wage per hour:" + ratePerHr);
+        System.out.println("Maximum working days:" + maxWorkingDays);
+        System.out.println("Maximum working hours:" + maxWorkinghrs);
+        return "Total wage for a month of " + company + " employee is " + totalEmpWage + "\n";
 
     }
     public static void main(String[] args)
     {
         System.out.println("Welcome to Employee Wage Computation Program....");
-        EmployeeWage wage = new EmployeeWage();
-        wage.calculateWage("TATA", 20, 20, 100);
-        wage.calculateWage("QK", 18, 20, 120);
-        wage.calculateWage("Reliance", 24, 22, 150);
+
+        EmployeeWage google = new EmployeeWage("Google", 20, 20, 100);
+        google.calculateWage();
+        System.out.println(google);
+
+        EmployeeWage microsoft = new EmployeeWage("Microsoft", 18, 20, 120);
+        microsoft.calculateWage();
+        System.out.println(microsoft);
+
 
     }
+
 }
